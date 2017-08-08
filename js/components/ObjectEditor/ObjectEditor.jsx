f2c28a151a64b0928dd65901b09ef1db04982995
Julie Pan <ycp217@nyu.edu>
2017-08-08 18:00:57 -0400

    styling

--- js/components/ObjectEditor/ObjectEditor.jsx
+++ js/components/ObjectEditor/ObjectEditor.jsx
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import dummydata from './data';
 import {fromJS, is, List, Map} from 'immutable';
+import TextField from 'material-ui/TextField';
+import IconButton from 'material-ui/IconButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import {grey300} from 'material-ui/styles/colors';
 
 // FETCH endpoint
 // PATCH endpoint
@@ -17,7 +21,8 @@ const Node = ({data, keyPath, onChange}) => {
   if (List.isList(data)) {
     nodes = data.map((child, i) => (
       <div style={{display: 'block'}} >
-      {i}: <Node keyPath={[...keyPath, i]} data={child} onChange={onChange} />
+        <span style={{color: 'green'}} >{i}: </span>
+        <Node keyPath={[...keyPath, i]} data={child} onChange={onChange} />
       </div>
       ));
     renderNodes = [
@@ -28,7 +33,8 @@ const Node = ({data, keyPath, onChange}) => {
   } else if (Map.isMap(data)) {
     nodes = data.entrySeq().map(([key, val]) => (
       <div style={{display: 'block'}} >
-      {key}: <Node keyPath={[...keyPath, key]} data={val} onChange={onChange} />
+        <span style={{color: 'blue'}} >{key}: </span>
+        <Node keyPath={[...keyPath, key]} data={val} onChange={onChange} />
       </div>
       ));
     renderNodes = [
@@ -60,10 +66,10 @@ class ObjectEditor extends Component {
   }
 
   onChange(keyPath, newValue) {
-    console.log(keyPath);
-    console.log(newValue);
+    // console.log(keyPath);
+    // console.log(newValue);
     const data = this.state.data.setIn(keyPath, newValue);
-    console.log(data.toJS());
+    // console.log(data.toJS());
     this.setState({data});
   }
 
@@ -73,7 +79,11 @@ class ObjectEditor extends Component {
       <div>
         EDITOR
       {!is(data, immudata) &&
-        <span style={{color: 'red'}} >CHANGED</span>}
+        <div>
+          <RaisedButton label='Save' primary />
+          <RaisedButton label='Revert' secondary onClick={_ => this.setState({data: immudata})} />
+        </div>
+      }
         <Node data={data} keyPath={[]} onChange={this.onChange} />
       </div>
     );
