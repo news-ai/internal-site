a6ef2551198e93ed8996e3d43484141616b2f5a7
Julie Pan <ycp217@nyu.edu>
2017-08-09 21:11:38 -0400

    add to existing obj

--- js/components/ObjectEditor/ObjectEditor.jsx
+++ js/components/ObjectEditor/ObjectEditor.jsx
@@ -5,7 +5,8 @@ import TextField from 'material-ui/TextField';
 import Dialog from 'material-ui/Dialog';
 import IconButton from 'material-ui/IconButton';
 import RaisedButton from 'material-ui/RaisedButton';
-import {grey300} from 'material-ui/styles/colors';
+import FontIcon from 'material-ui/FontIcon';
+import {grey300, grey500} from 'material-ui/styles/colors';
 import cn from 'classnames';
 import styled from 'styled-components';
 import map from './schema';
@@ -26,7 +27,7 @@ const convert = mapping => Object
   return newMap;
 }, {});
 
-const mapping = convert(map.data.md1.mappings.contacts.properties);
+const mapping = convert(map.data.md1.mappings.contacts.properties.data.properties);
 
 const ObjectBlock = styled.div`
   display: ${props => props.inline ? 'flex' : 'block'};
@@ -69,22 +70,47 @@ class Node extends Component {
   }
 
   render() {
-    const {data, keyPath, onChange, handleAddClick, schema} = this.props;
+    const {data, keyPath, onChange, handleAddClick, handleRemoveClick, schema} = this.props;
     const {open} = this.state;
     let renderNodes = [];
     let nodes = [];
+
+    // console.log(keyPath);
+    // console.log(schema);
+    const keyType = keyPath
+    .filter(path => typeof path === 'string')
+    .reduce((acc, path, i) => acc[path], schema);
+    // console.log(keyType);
+    // console.log(keyPath);
 
     if (List.isList(data)) {
       nodes = data.map((child, i) => {
         return (
           <div style={{display: 'block', marginLeft: 10}} >
-            <label style={{color: 'green'}} >{i}: </label>
-            <Node schema={schema} keyPath={[...keyPath, i]} data={child} onChange={onChange} handleAddClick={handleAddClick} />
+            <FontIcon
+            onClick={_ => handleRemoveClick([...keyPath, i])}
+            color={grey300}
+            hoverColor={grey500}
+            className='fa fa-times'
+            style={{fontSize: '0.9em', marginRight: 5}}
+            />
+            <span className='text' style={{color: 'green'}} >{i}: </span>
+            <Node
+            schema={schema}
+            keyPath={[...keyPath, i]}
+            data={child}
+            onChange={onChange}
+            handleAddClick={handleAddClick}
+            handleRemoveClick={handleRemoveClick}
+            />
           </div>
           );
       });
       renderNodes = open ? [
-        <GreenDiv onClick={this.onClose} >[ </GreenDiv>,
+        <GreenDiv>
+          <FontIcon onClick={_ => handleAddClick(keyPath, keyType)} color={grey300} hoverColor={grey500} className='fa fa-plus' style={{fontSize: '0.9em', marginRight: 5}} />
+          <span onClick={this.onClose}>[ </span>
+        </GreenDiv>,
         ...nodes,
         <GreenDiv onClick={this.onClose} > ]</GreenDiv>,
       ] : <GreenDiv onClick={this.onOpen} >[ ... ]</GreenDiv>;
@@ -93,29 +119,54 @@ class Node extends Component {
         const isProperty = !Map.isMap(val) && !List.isList(val);
         return (
         <ObjectBlock inline={isProperty} >
+          <FontIcon
+          onClick={_ => handleRemoveClick([...keyPath, key])}
+          color={grey300}
+          hoverColor={grey500}
+          className='fa fa-times pointer'
+          style={{fontSize: '0.9em', marginRight: 5}}
+          />
           <span style={{color: 'blue'}} >{key}: </span>
-          <Node schema={schema} keyPath={[...keyPath, key]} data={val} onChange={onChange} handleAddClick={handleAddClick} />
+          <Node
+          schema={schema}
+          keyPath={[...keyPath, key]}
+          data={val}
+          onChange={onChange}
+          handleAddClick={handleAddClick}
+          handleRemoveClick={handleRemoveClick}
+          />
         </ObjectBlock>
         );
       });
+      const numKeyUnfilled = Object.keys(keyType).length - data.keySeq().filter(key => keyType[key]).count();
       renderNodes = open ? [
-        <BlueDiv onClick={this.onClose} >{'{ '}</BlueDiv>,
+        <BlueDiv >
+          <span onClick={this.onClose}>{'{ '}</span>
+        {numKeyUnfilled > 0 &&
+          <FontIcon
+          onClick={_ => handleAddClick(keyPath, keyType)}
+          color={grey300}
+          hoverColor={grey500}
+          className='fa fa-plus'
+          style={{fontSize: '0.9em', marginLeft: 5}}
+          />}
+        </BlueDiv>,
         ...nodes,
         <BlueDiv onClick={this.onClose} > {' }'}</BlueDiv>,
       ] : <BlueDiv onClick={this.onOpen} >{'{ ... }'}</BlueDiv>;
     } else {
-      // console.log(keyPath);
-      // console.log(schema);
-      const type = keyPath
-      .filter(path => typeof path === 'string')
-      .reduce((acc, path, i) => {
-        return acc[path];
-      }, schema);
-      // console.log(type);
       if (data === null) {
-        return <div className='pointer' style={{marginLeft: 10}} onClick={_ => handleAddClick(keyPath, type)} >null</div>;
+        return <div className='pointer' style={{marginLeft: 10}} onClick={_ => handleAddClick(keyPath, keyType)} >null</div>;
       }
-      return <TextField id={keyPath.join('-')} style={{marginLeft: 10}} value={data} onChange={e => onChange(keyPath, e.target.value)} />;
+      return (
+        <TextField
+        placeholder={keyType.type}
+        id={keyPath.join('-')}
+        style={{marginLeft: 10}}
+        value={data}
+        onChange={e => onChange(keyPath, e.target.value)}
+        />
+        );
     }
     return (
       <NodeContainer>
@@ -125,12 +176,18 @@ class Node extends Component {
   }
 }
 
-const KeyTypeObject = ({keyType, parentName, keyPath, onChange}) => {
-  if (keyType.type) {
+const KeyTypeObject = ({originalKeyPath, keyType, parentName, keyPath, onChange, data}) => {
+  if (typeof keyType.type === 'string') {
     return (
       <div>
         <span>{parentName}: </span>
-        <TextField id={parentName} placeholder={keyType.type} type='text' onChange={e => onChange(keyPath, e.target.value)} />
+        <TextField
+        id={parentName}
+        disabled={data.getIn(originalKeyPath).has(parentName)}
+        placeholder={keyType.type}
+        type='text'
+        onChange={e => onChange(keyPath, e.target.value)}
+        />
       </div>
       );
   }
@@ -142,6 +199,8 @@ const KeyTypeObject = ({keyType, parentName, keyPath, onChange}) => {
     parentName={typeName}
     keyType={keyType[typeName]}
     onChange={onChange}
+    data={data}
+    originalKeyPath={originalKeyPath}
     />);
 
   return (
@@ -156,8 +215,8 @@ class ObjectEditor extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: fromJS(dummydata),
-      immudata: fromJS(dummydata),
+      data: fromJS(dummydata.data),
+      immudata: fromJS(dummydata.data),
       open: false,
       keyPath: null,
       keyType: null
@@ -166,6 +225,8 @@ class ObjectEditor extends Component {
     this.onAddSubmit = this.onAddSubmit.bind(this);
     this.handleAddClick = this.handleAddClick.bind(this);
     this.onTempMapChange = this.onTempMapChange.bind(this);
+    this.onCloseAddPanel = _ => this.setState({open: false, keyPath: null, keyType: null});
+    this.handleRemoveClick = this.handleRemoveClick.bind(this);
   }
 
   onChange(keyPath, newValue) {
@@ -177,9 +238,20 @@ class ObjectEditor extends Component {
   }
 
   onAddSubmit() {
-    const mergeObj = this.whichObject.value === 'array' ? List([this.state.tempMap]) : this.state.tempMap;
-    const data = this.state.data.setIn(this.state.keyPath, mergeObj);
-    this.setState({data});
+    const existingData = this.state.data.getIn(this.state.keyPath);
+    let data;
+    if (existingData === null) {
+      // add complete new data to previously null field
+      const mergeObj = this.whichObject.value === 'array' ? List([this.state.tempMap]) : this.state.tempMap;
+      data = this.state.data.setIn(this.state.keyPath, mergeObj);
+    } else if (List.isList(existingData)) {
+      // append existing List with new data
+      const newList = existingData.push(this.state.tempMap);
+      data = this.state.data.setIn(this.state.keyPath, newList);
+    } else if (Map.isMap(existingData)) {
+      data = this.state.data.setIn(this.state.keyPath, existingData.merge(this.state.tempMap));
+    }
+    this.setState({data}, this.onCloseAddPanel);
   }
 
   onTempMapChange(keyPath, newValue) {
@@ -188,7 +260,14 @@ class ObjectEditor extends Component {
   }
 
   handleAddClick(keyPath, keyType) {
+    console.log(keyPath);
+    console.log(keyType);
     this.setState({keyPath, keyType, tempMap: Map({})}, _ => this.setState({open: true}));
+  }
+
+  handleRemoveClick(keyPath) {
+    const data = this.state.data.deleteIn(keyPath);
+    this.setState({data});
   }
 
   render() {
@@ -201,7 +280,7 @@ class ObjectEditor extends Component {
           <RaisedButton label='Revert' secondary onClick={_ => this.setState({data: immudata})} />
           <RaisedButton label='Save' primary />
         </div>}
-        <Dialog open={open} onRequestClose={_ => this.setState({open: false, keyPath: null, keyType: null})} >
+        <Dialog open={open} onRequestClose={this.onCloseAddPanel} >
         {!!keyPath && !!keyType &&
           <div>
             <div>
@@ -216,14 +295,28 @@ class ObjectEditor extends Component {
               </div>
             </div>
             <div>
-              <KeyTypeObject onChange={this.onTempMapChange} keyPath={[]} parentName={keyPath[keyPath.length - 1]} keyType={keyType} />
+              <KeyTypeObject
+              data={data}
+              onChange={this.onTempMapChange}
+              originalKeyPath={keyPath}
+              keyPath={[]}
+              parentName={keyPath[keyPath.length - 1]}
+              keyType={keyType}
+              />
             </div>
             <div className='right'>
               <RaisedButton primary label='Submit' onClick={this.onAddSubmit} />
             </div>
           </div>}
         </Dialog>
-        <Node schema={mapping} data={data} keyPath={[]} onChange={this.onChange} handleAddClick={this.handleAddClick} />
+        <Node
+        schema={mapping}
+        data={data}
+        keyPath={[]}
+        onChange={this.onChange}
+        handleAddClick={this.handleAddClick}
+        handleRemoveClick={this.handleRemoveClick}
+        />
       </div>
     );
   }
